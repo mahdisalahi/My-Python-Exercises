22f4aba2296a8cfc6ab887519ef0e20a437920bb
Mr.Salahinia <salahimahdi938@gmail.com>
2022-12-30 22:17:30 +0330

    GetYearAndComputing

---
+++ MonthWeekDayAccordingToYearOfBirth/Train02.py
@@ -0,0 +1,7 @@
+year = int(input("Enter your Year of Birth: "))
+
+month = year * 12
+week = month * 4
+day = week * 7
+
+print("Year: %s | Month: %s | Week: %s | Day: %s" % (year, month, week, day))
