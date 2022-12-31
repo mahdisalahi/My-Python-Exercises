edccf016c5a1a8b67e2280140f63c85be43945d4
Mr.Salahinia <salahimahdi938@gmail.com>
2022-12-31 23:34:05 +0330

    AddSimpleValidation

--- MonthWeekDayAccordingToYearOfBirth/Train02.py
+++ MonthWeekDayAccordingToYearOfBirth/Train02.py
@@ -1,7 +1,14 @@
-year = int(input("Enter your Year of Birth: "))
+year = input("Enter your Year of Birth: ")
 
-month = year * 12
+yearLen = len(year)
+
+if yearLen != 4:
+    raise TypeError("Year of birth must be 4 digits")
+else:
+    yearInt = int(year)
+
+month = yearInt * 12
 week = month * 4
 day = week * 7
 
-print("Year: %s | Month: %s | Week: %s | Day: %s" % (year, month, week, day))
+print("Year: %s | Month: %s | Week: %s | Day: %s" % (yearInt, month, week, day))
